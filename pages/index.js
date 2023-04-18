f618e0ca6684d4bf051feb00c81bb4356f582626
ammar <ammar.ali.haidar.1990@gmail.com>
2023-04-18 09:23:47 +0300

    logo

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({data}) {
         <title>Amazon 2.0</title>
         <meta name="description" content="Amazon clone" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/small-logo.png" />
       </Head>
 <Header />
 <main className='max-w-screen-2xl mx-auto'>
